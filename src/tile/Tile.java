32debafb2582c19460bdc80af42dfeb5f51710ad
simonescano <116249121+simon-escano@users.noreply.github.com>
2023-12-08 22:32:11 +0800

    Update Tile.java

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -1,2 +1,24 @@
-package tile;public class Tile {
+package tile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Tile {
+    public BufferedImage image;
+    public boolean collision = false;
+
+    public Tile(String filename) {
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/" + filename)));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Tile(String filename, boolean collision) {
+        this(filename);
+        this.collision = collision;
+    }
 }
